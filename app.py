e5d194797a8bf552d1bac7da8f4c3d5bb605f6f1
kcaruso713 <kclabby713@icloud.com>
2025-01-26 04:05:56 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ df = pd.read_csv('vehicles_us_2.csv')
 df.drop_duplicates # Check for duplicates
 df = df.drop_duplicates() # Drop duplicates
 
-df.duplicates().sum() # Check for duplicates
+df.duplicated().sum() # Check for duplicates
 
 
 
