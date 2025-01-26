472f1359c66248d134828cce34d05e5e032ed7aa
kcaruso713 <kclabby713@icloud.com>
2025-01-25 23:26:40 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -1,2 +1,12 @@
-import streamlist as st
-import pandas as pd
+import streamlit as st # type: ignore
+import pandas as pd # type: ignore
+import plotly.express as px # type: ignore
+
+# Ensure the dataset path is relative to the root directory.
+df = pd.read_csv('vehicles_us.csv')
+
+st.header("My Data Dashboard")
+
+# Example of adding a chart
+fig = px.histogram(df, x='price')
+st.plotly_chart(fig)
